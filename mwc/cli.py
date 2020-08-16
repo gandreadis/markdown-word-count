da7eafdac71d1d14d735915010b6c053c0a17a7f
Alexandre Augusto <lexander.santos@gmail.com>
2020-08-16 18:55:34 -0300

    Check if file exists before running anything

--- mwc/cli.py
+++ mwc/cli.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from os.path import join, isfile, isdir
+from os.path import join, isfile, isdir, exists
 from os import listdir
 import os
 import sys
@@ -18,15 +18,28 @@ def main():
         print('Provide the Markdown file or folder to parse as first argument')
         sys.exit(1)
 
+    if not exists(sys.argv[1]):
+        print('File or folder not found.')
+        sys.exit(1)
+
     if isdir(sys.argv[1]):
-        allfiles = [join(sys.argv[1], f) for f in listdir(sys.argv[1]) if isfile(join(sys.argv[1], f)) and f.endswith(".md")]
+        allfiles = [join(sys.argv[1], f) for f in listdir(sys.argv[1]) if isfile(join(sys.argv[1], f)) and f.endswith('.md')]
+        allfiles.sort()
+        response = str()
         for file in allfiles:
             with open(file, 'r', encoding='utf8') as f:
-                print(file, count_words_in_markdown(f.read()))
-        return 0
+                r = '{0} - {1}\n'.format(file, count_words_in_markdown(f.read()))
+                response += r
+        print(response)
+        return response
 
     with open(sys.argv[1], 'r', encoding='utf8') as f:
-        print(count_words_in_markdown(f.read()))
+        if not sys.argv[1].endswith('.md'):
+            print('The file provided in the first argument is not a Markdown file.')
+            sys.exit(1)
+        response = '{0} - {1}'.format(sys.argv[1], count_words_in_markdown(f.read()))
+        print(response)
+        return response
 
 
 if __name__ == '__main__':
